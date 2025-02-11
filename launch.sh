8a9a904f4c3d27581df5f0596944fc01b707304b
Doridian <git@doridian.net>
2025-02-10 16:55:55 -0800

    fix

--- launch.sh
+++ launch.sh
@@ -5,8 +5,8 @@ export SHIP_HOME="${HOME}/.config/n64/sf64"
 export SDL_VIDEODRIVER=wayland
 mkdir -p "${SHIP_HOME}"
 
-if [ ! -f "${SHIP_HOME}/sf64.otr" ]; then
-  ln -sf /opt/n64/sf64-git/sf64.otr "${SHIP_HOME}/sf64.otr"
+if [ ! -f "${SHIP_HOME}/sf64.o2r" ]; then
+  ln -sf /opt/n64/sf64-git/sf64.o2r "${SHIP_HOME}/sf64.o2r"
 fi
 
 cd "${SHIP_HOME}"
